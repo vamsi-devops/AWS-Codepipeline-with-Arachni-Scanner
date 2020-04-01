bd34bb4aab103a38c13ee3389406b439ac52bd5e
vamsi-devops <60419553+vamsi-devops@users.noreply.github.com>
2020-04-01 12:04:55 +0530

    Update starttomcat.sh

--- scripts/starttomcat.sh
+++ scripts/starttomcat.sh
@@ -2,6 +2,6 @@
 sudo service tomcat9 start
 sudo service tomcat9 restart
 cd /root/arachni-1.5.1-0.5.12/bin
-touch f3
-./arachni http://13.112.218.94:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* > f3.html
-aws s3 cp f3.html s3://archni/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers 
+touch f4.html
+./arachni http://13.112.218.94:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* > f4.html
+aws s3 cp f4.html s3://archni/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers 
